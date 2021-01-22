062b807e4043dbc81fd93d19b6c9404d07c4dfc7
Your Name <you@example.com>
2021-01-22 18:42:48 +0530

    VGA printing complete with macros

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 #![no_std]
 #![no_main]
 
+mod mods;
+
 use core::fmt::Write;
 use core::panic::PanicInfo;
-mod vga;
-use vga::*;
+
+use mods::*;
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
@@ -13,16 +15,5 @@ fn panic(_info: &PanicInfo) -> ! {
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    // print!("Hello!");
-
-    let mut w = vga::Writer {
-        colp: 0,
-        rowp: 0,
-        buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
-        color_code: 0xf,
-    };
-
-    write!(w, "{}", 1.0 / 3.0).unwrap();
-
     loop {}
 }
