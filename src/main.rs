01ab13cba645dca1d864b750690ce0c84e05bf64
Rishit Khandelwal <rishitkh@gmail.com>
2021-07-10 14:06:12 +0530

    more paging

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,15 @@
 use core::panic::PanicInfo;
 
 use rkos::{
-    memory::active_level_4_table,
-    println,
+    memory::{self},
     vga::{Charecter, Color, ColorCode, BUFFER_HEIGHT, BUFFER_WIDTH, WRITER},
 };
 
 use bootloader::{entry_point, BootInfo};
-use x86_64::VirtAddr;
+use x86_64::{
+    structures::paging::{Page, Translate},
+    VirtAddr,
+};
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
@@ -47,12 +49,17 @@ fn kernel_main(bootinfo: &'static BootInfo) -> ! {
     unsafe { WRITER.force_unlock() };
 
     let phys_mem_offset = VirtAddr::new(bootinfo.physical_memory_offset);
-    let l4_table = unsafe { active_level_4_table(phys_mem_offset) };
 
-    for (i, entry) in l4_table.iter().enumerate() {
-        if !entry.is_unused() {
-            println!("L4 entry {}: {:?}", i, entry);
-        }
+    let mut mapper = unsafe { memory::init(phys_mem_offset) };
+    let mut frame_allocator = memory::EmptyFrameAllocator;
+
+    let page = Page::containing_address(VirtAddr::new(0xdeadbeaf000));
+
+    memory::create_example_mapping(page, &mut mapper, &mut frame_allocator);
+
+    let page_ptr: *mut u64 = page.start_address().as_mut_ptr();
+    unsafe {
+        page_ptr.offset(400).write_volatile(0x_f021_f077_f065_f04e);
     }
 
     loop {
